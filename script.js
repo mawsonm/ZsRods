7e289fb51d681845153e64c554d2a7c1e677ef99
mawsonm <mawsonm@umich.edu>
2021-01-03 15:23:00 -0500

    new repository to initialize instagram api

---
+++ script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB8flqkTgvw8i6CtaQQ-FV7vdMGuGkTvwE",
+    authDomain: "z-s-rods.firebaseapp.com",
+    databaseURL: "https://z-s-rods-default-rtdb.firebaseio.com",
+    projectId: "z-s-rods",
+    storageBucket: "z-s-rods.appspot.com",
+    messagingSenderId: "551381686100",
+    appId: "1:551381686100:web:2adb40ce85af9f1e59dac1",
+    measurementId: "G-0RL3WY7HQQ"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let database = firebase.database();
